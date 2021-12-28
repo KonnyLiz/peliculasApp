87dab528ba4bb22ca4ad41c20adbfd20f20319c3
Konny Amaya <konnylizamaya@gmail.com>
2021-12-27 21:15:54 -0600

    search desde el navbar

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PeliculasLista } from 'src/app/models/peliculas.interface';
 import { Resultados } from 'src/app/models/resultados.interface';
 import { PeliculasService } from 'src/app/services/peliculas.service';
@@ -12,11 +13,14 @@ export class BuscarComponent implements OnInit {
   lista: PeliculasLista[] = [];
 
   constructor(
-    private _ps: PeliculasService
+    private _ps: PeliculasService,
+    private actRoute: ActivatedRoute
   ) {
-    _ps.getBuscar('evan').subscribe((res) => {
-      this.lista = res.results;
-      console.log(res.results);
+    actRoute.params.subscribe(txt => {
+      _ps.getBuscar(txt['txt']).subscribe((res) => {
+        this.lista = res.results;
+        console.log(res.results);
+      });
     });
   }
 
